a7e159170d3e653672c664b103cb91037ba060d3
pekr0002 <pekr0002@stud.kea.dk>
2025-11-16 16:26:31 +0100

    singleview styling v1

--- src/app/product/[id]/page.js
+++ src/app/product/[id]/page.js
@@ -1,6 +1,6 @@
 import { Suspense } from "react";
 import Image from "next/image";
-import { FaRegStar } from "react-icons/fa6";
+import CategoriesBanner from "@/app/components/CategoriesBanner";
 
 const details = async ({ params }) => {
   const { id } = await params;
@@ -9,58 +9,76 @@ const details = async ({ params }) => {
 
   return (
     <Suspense>
-      <div className="max-w-[1000px]">
-        <div className="grid grid-cols-2">
-          <Image
-            src={product.images[0]}
-            alt={product.title}
-            width={200}
-            height={200}
-            loading="eager"
-            className="flex h-auto w-full items-center justify-center"
-          />
+      <>
+        <div className="mx-auto my-0 grid max-w-[1000px] grid-cols-2 gap-8 py-10">
+          <div className="m-2 w-full border-2 border-solid">
+            <Image
+              src={product.images[0]}
+              alt={product.title}
+              width={500}
+              height={500}
+              loading="eager"
+              className="flex h-auto w-full items-center justify-center"
+            />
+          </div>
           <div>
-            <h1>{product.title}</h1>
-            <p>{product.description}</p>
-            <p>{product.price},-</p>
-            <p>{product.rating}★</p>
-            <p>{product.brand}</p>
-            <p>{product.availabilityStatus}</p>
-            <p>{product.shippingInformation}</p>
+            <p className="my-1.5 font-(family-name:--font-dm-mono) text-3xl">
+              {product.brand}
+            </p>
+            <h1 className="my-2.5 text-4xl">{product.title}</h1>
+            <p className="my-1.5 font-(family-name:--font-dm-mono) text-2xl">
+              {product.price},-
+            </p>
+            <p className="my-1.5">{product.description}</p>
+            <p className="my-4.5">{product.availabilityStatus}</p>
 
-            <button
-              className="px-8 py-2 text-(--light)"
-              style={{ background: "var(--accent)" }}
-            >
+            <button className="hoverInvert border-2 border-(--background) bg-(--foreground) px-8 py-2 font-(family-name:--font-dm-mono) text-(--background) uppercase">
               Add to Cart
             </button>
-            {/* mono font */}
-            <p className="">{product.returnPolicy}</p>
+            <div className="my-4">
+              <p className="my-1.5">{product.shippingInformation}</p>
+              <p className="my-1.5">{product.returnPolicy}</p>
+              <p className="my-1.5">{product.warrantyInformation}</p>
+            </div>
           </div>
         </div>
 
-        <div>
-          <h2 className="text-4xl">Reviews</h2>
-          {/* mono */}
-          <div className="flex flex-wrap justify-between gap-4">
-            {product.reviews.map((review, index) => (
-              <div key={index}>
-                <div className="flex">
-                  {[...Array(review.rating)].map((_, i) => (
-                    <FaRegStar key={i} color="var(--dark)" size={24} />
-                  ))}
+        <div className="h-fit bg-(--dark) py-10">
+          <div className="mx-auto h-fit max-w-[1000px]">
+            <div className="my-8">
+              <h2 className="my-4 text-4xl text-(--background)">Reviews</h2>
+              <p className="my-1.5 text-3xl text-(--background) uppercase">
+                {product.rating}★ overall rating
+              </p>
+            </div>
+            {/* mono */}
+            <div className="special flex flex-wrap justify-between gap-4 text-(--background)">
+              {product.reviews.map((review, index) => (
+                <div key={index}>
+                  <div className="flex">
+                    {[...Array(review.rating)].map((_, i) => (
+                      <p key={i} className="text-3xl text-(--background)">
+                        ★
+                      </p>
+                    ))}
+                  </div>
+
+                  <p className="text-[24px] text-(--background)">
+                    {review.comment}
+                  </p>
+                  <p className="text-[20px] text-(--background)">
+                    {review.reviewerName}
+                  </p>
+                  <p className="text-[20px] text-(--background)">
+                    {new Date(review.date).toLocaleDateString()}
+                  </p>
                 </div>
-
-                <p className="text-[24px]">{review.comment}</p>
-                <p className="text-[20px]">{review.reviewerName}</p>
-                <p className="text-[20px]">
-                  {new Date(review.date).toLocaleDateString()}
-                </p>
-              </div>
-            ))}
+              ))}
+            </div>
           </div>
         </div>
-      </div>
+        <CategoriesBanner />
+      </>
     </Suspense>
   );
 };
